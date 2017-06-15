08dbe2cd5316596d433be5c20e7aa11a59930dc9
Olivier Bruchez <olivier@bruchez.org>
2017-06-15 19:09:53 +0200

    Add code to log file tree content

--- src/main/scala/org/bruchez/olivier/msatozip/MsaToZip.scala
+++ src/main/scala/org/bruchez/olivier/msatozip/MsaToZip.scala
@@ -13,35 +13,18 @@ import scala.collection.JavaConverters._
 object MsaToZip {
   // scalastyle:off method.length
   def main(args: Array[String]): Unit = {
-    Batch.convertToZip(Paths.get("/Users/olivierbruchez/Downloads/MSA"))
-    System.exit(0)
+    //Batch.convertToZip(Paths.get("/Users/olivierbruchez/Downloads/MSA"))
+    //System.exit(0)
 
-    //val msaImage = MsaImage(Paths.get("/Users/olivierbruchez/Downloads/atari37.msa"))
-    val msaImage = MsaImage(Paths.get("/Users/olivierbruchez/Downloads/olivier1.msa"))
+    val msaPath = Paths.get("/Users/olivierbruchez/Downloads/MSA test images/olivier1.msa")
+    //val msaPath = Paths.get("/Users/olivierbruchez/Downloads/MSA test images/atari37.msa")
+    val msaImage = MsaImage(msaPath)
 
     val atariFilesystem = new AtariFilesystem(msaImage)
 
     val fileTree = atariFilesystem.fileTree()
 
-    def dump(directory: Directory, level: Int = 0): Unit = {
-      for {
-        child <- directory.children
-      } {
-        print("  " * level + child.name + child.dateTime.map(dt => s" ($dt)").getOrElse(""))
-
-        child match {
-          case _: File =>
-            println()
-          case _: CorruptedFile =>
-            println(" (corrupted)")
-          case d: Directory =>
-            println()
-            dump(d, level = level + 1)
-        }
-      }
-    }
-
-    dump(fileTree)
+    fileTree.saveLogs(Paths.get(msaPath.toString + ".log"))
 
     System.exit(0)
 
--- src/main/scala/org/bruchez/olivier/msatozip/tree/File.scala
+++ src/main/scala/org/bruchez/olivier/msatozip/tree/File.scala
@@ -1,29 +1,72 @@
 package org.bruchez.olivier.msatozip.tree
 
+import java.nio.file._
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 import org.bruchez.olivier.msatozip.fat.Corruption
 
 sealed trait DirectoryOrFile {
   def name: String
   def dateTime: Option[LocalDateTime]
+  def logs(level: Int): Seq[String]
+
+  def saveLogs(path: Path): Unit =
+    Files.write(
+      path,
+      logs(level = 0).mkString("\n").getBytes("utf-8"),
+      StandardOpenOption.CREATE,
+      StandardOpenOption.TRUNCATE_EXISTING
+    )
+
+  protected def logLine(level: Int, sizeOption: Option[Int]): String = {
+    val dateTimeString = dateTime.map(formatter.format).getOrElse(NoDateTimeString)
+    val sizeString = sizeOption match {
+      case Some(size) =>
+        val MaxRawSizeString = 7
+        val rawSizeString = size.toString
+        " " * (MaxRawSizeString - rawSizeString.size) + rawSizeString
+      case None =>
+        ""
+    }
+    val nameString = s" $name"
+
+    ("  " * level) + dateTimeString + sizeString + nameString
+  }
+
+  private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+  private val NoDateTimeString = "????-??-?? ??:??:??"
 }
 
 case class Directory(
-  override val name: String,
-  override val dateTime: Option[LocalDateTime],
-  children: Seq[DirectoryOrFile]
-) extends DirectoryOrFile
+    override val name: String,
+    override val dateTime: Option[LocalDateTime],
+    children: Seq[DirectoryOrFile]
+) extends DirectoryOrFile {
+  override def logs(level: Int): Seq[String] = {
+    val rootDirectory = name.isEmpty
+
+    val currentDirectoryLogs = if (rootDirectory) Seq() else Seq(logLine(level, sizeOption = None))
+    val childrenLevel = if (rootDirectory) level else level + 1
+    val childrenLogs = children.flatMap(_.logs(childrenLevel))
+
+    currentDirectoryLogs ++ childrenLogs
+  }
+}
 
 case class File(
-  override val name: String,
-  override val dateTime: Option[LocalDateTime],
-  data: Seq[Byte]
-) extends DirectoryOrFile
+    override val name: String,
+    override val dateTime: Option[LocalDateTime],
+    data: Seq[Byte]
+) extends DirectoryOrFile {
+  override def logs(level: Int): Seq[String] = Seq(logLine(level, sizeOption = Some(data.size)))
+}
 
 case class CorruptedFile(
-  override val name: String,
-  override val dateTime: Option[LocalDateTime],
-  data: Seq[Byte],
-  corruption: Corruption
-) extends DirectoryOrFile
+    override val name: String,
+    override val dateTime: Option[LocalDateTime],
+    data: Seq[Byte],
+    corruption: Corruption
+) extends DirectoryOrFile {
+  override def logs(level: Int): Seq[String] = Seq(logLine(level, sizeOption = Some(data.size)) + " (corrupted)")
+}
